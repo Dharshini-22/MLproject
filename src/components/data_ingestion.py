df4051770a86c84c5cad6127eb92f7ffe3cc4ebc
Dharshini Sudarsan <sdharshu99@gmail.com>
2023-12-02 10:06:46 +0530

    Data Transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,8 @@ from dataclasses import dataclass
 
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 
 @dataclass 
 class DataIngestionConfig:
@@ -56,7 +58,9 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
